d0b71cabd1c0525dbad9144d876c413b6af62b8f
jesushf <jehernandez1996@gmail.com>
2017-12-18 01:01:15 +0100

    curling scene added and soft changes in level loader and vs config

--- src/LevelLoader.cpp
+++ src/LevelLoader.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 
 LevelLoader::LevelLoader(){
-
+    
 }
 
 bool LevelLoader::LoadLevel(std::string jsonPath){
@@ -32,11 +32,11 @@ bool LevelLoader::LoadLevel(std::string jsonPath){
         //pointer to object body
         auto ptr = j["Objects"][i]["Body"];
         //unity transform
-         position = vector3df(ptr["Position"][0], ptr["Position"][1], ptr["Position"][2]);
-         rotation = vector3df(ptr["Rotation"][0], ptr["Rotation"][1], ptr["Rotation"][2]);
-         size     = vector3df(ptr["Scale"][0], ptr["Scale"][1], ptr["Scale"][2]);
+        position = vector3df(ptr["Position"][0], ptr["Position"][1], ptr["Position"][2]);
+        rotation = vector3df(ptr["Rotation"][0], ptr["Rotation"][1], ptr["Rotation"][2]);
+        size     = vector3df(ptr["Scale"][0], ptr["Scale"][1], ptr["Scale"][2]);
 
-        //Textures and objects        
+        //Textures and objects
         texture = ptr["Texture"];
         model = ptr["3DModel"];
 
@@ -48,16 +48,16 @@ bool LevelLoader::LoadLevel(std::string jsonPath){
         if(j["Objects"][i]["Type"] == "Block"){
             objManager->AddBlock(position, size, rotation, texture);           
         }
+        else if(j["Objects"][i]["Type"] == "Switch"){
+            // do nothing here
+        }
         else if(j["Objects"][i]["Type"] == "Door"){
             int idDoor = j["Objects"][i]["ID"];
             doors[idDoor] = objManager->AddDoor(position, size, rotation, axis);
         }
-        else if(j["Objects"][i]["Type"] == "Switch"){
-            //int idDoor = j["Objects"][i]["Door"];            
-            //objManager->AddSwitch(doors[idDoor], position, size, rotation, axis);
-        }
         else if(j["Objects"][i]["Type"] == "PotionSpawner"){
-            objManager->AddBlock(position, size, rotation, "../assets/textures/red.jpg");           
+            objManager->AddBlock(position, size, rotation, "../assets/textures/red.jpg");
+            position.Y += 1;
             objManager->AddPotion(position, vector3df(0.25,0.25,0.25), rotation);
         }
 		else if(j["Objects"][i]["Type"] == "Potion"){
@@ -72,13 +72,36 @@ bool LevelLoader::LoadLevel(std::string jsonPath){
         else if(j["Objects"][i]["Type"] == "Fountain"){
             objManager->AddFountain(position, vector3df(1,1,1), rotation);
         }
-        else if(j["Objects"][i]["Type"] == "MainCamera"){
-            //std::cout<<"MainCamera, pos: "<<position<<std::endl;        
+        else if(j["Objects"][i]["Type"] == "NPC1"){
+            //objManager->AddNPC(position, vector3df(1,1,1), rotation, 1);
+        }
+        else if(j["Objects"][i]["Type"] == "NPC2"){
+            //objManager->AddNPC(position, vector3df(1,1,1), rotation, 2);
+        }
+        else if(j["Objects"][i]["Type"] == "NPC3"){
+            //objManager->AddNPC(position, vector3df(1,1,1), rotation, 3);
         }
         else{
             //std::cout<<"No se controla el tipo: "<<j["Objects"][i]["Type"]<<std::endl;
         }
     }
 
+    // loop to iterate switch objects and assign it to a door
+    for(int i = 0; !j["Objects"][i].is_null(); i++){
+        if(j["Objects"][i]["Type"] == "Switch" && !j["Objects"][i]["TargetDoor"].is_null()){
+            //pointer to object body
+            auto ptr = j["Objects"][i]["Body"];
+            //unity transform
+            position = vector3df(ptr["Position"][0], ptr["Position"][1], ptr["Position"][2]);
+            rotation = vector3df(ptr["Rotation"][0], ptr["Rotation"][1], ptr["Rotation"][2]);
+            size     = vector3df(ptr["Scale"][0], ptr["Scale"][1], ptr["Scale"][2]);
+
+            //axis = ptr["AxisCoord"].empty()? vector3df() : 
+            //vector3df(ptr["AxisCoord"][0], ptr["AxisCoord"][1], ptr["AxisCoord"][2]);
+                        
+            int assignedDoorID = j["Objects"][i]["TargetDoor"];
+            objManager->AddSwitch(doors[assignedDoorID], position, vector3df(1,1,1), rotation, vector3df(0,0,0));
+        }
+    }
     return true;
 }
