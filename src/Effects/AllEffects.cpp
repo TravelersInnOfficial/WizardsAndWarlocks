3e8fc4c0bf2bdc5b5ffe41c48e8dc6958a0a8de0
DanielFinestrat <danielfinestratmartinez@hotmail.com>
2017-12-20 20:42:58 +0100

    alleffects hotfix

--- src/Effects/AllEffects.cpp
+++ src/Effects/AllEffects.cpp
@@ -107,3 +107,99 @@ void Madness::RemoveEffect(Player* p){
 ////////////////////////////////////////////
 ///	UPGRADES 	////////////////////////////
 ////////////////////////////////////////////
+
+//================================================================
+// DAMAGE UP
+//================================================================
+DamageUp::DamageUp(float time):Effect(time){
+}
+
+void DamageUp::ApplyEffect(Player* p){
+}
+
+void DamageUp::RemoveEffect(Player* p){
+}
+
+//================================================================
+// DEFENSE UP
+//================================================================
+DefenseUp::DefenseUp(float time):Effect(time){
+}
+
+void DefenseUp::ApplyEffect(Player* p){
+}
+
+void DefenseUp::RemoveEffect(Player* p){
+}
+
+//================================================================
+// ELEMENT UP
+//================================================================
+ElementUp::ElementUp(float time):Effect(time){
+}
+
+void ElementUp::ApplyEffect(Player* p){
+}
+
+void ElementUp::RemoveEffect(Player* p){
+}
+
+//================================================================
+// SPEED UP
+//================================================================
+SpeedUp::SpeedUp(float time):Effect(time){
+}
+
+void SpeedUp::ApplyEffect(Player* p){
+}
+
+void SpeedUp::RemoveEffect(Player* p){
+}
+
+//================================================================
+// UNTARGETABLE
+//================================================================
+Untargetable::Untargetable(float time):Effect(time){
+}
+
+void Untargetable::ApplyEffect(Player* p){
+}
+
+void Untargetable::RemoveEffect(Player* p){
+}
+
+//================================================================
+// FIRE SHOTS
+//================================================================
+FireShots::FireShots(float time):Effect(time){
+}
+
+void FireShots::ApplyEffect(Player* p){
+}
+
+void FireShots::RemoveEffect(Player* p){
+}
+
+//================================================================
+// POISON SHOTS
+//================================================================
+PoisonShots::PoisonShots(float time):Effect(time){
+}
+
+void PoisonShots::ApplyEffect(Player* p){
+}
+
+void PoisonShots::RemoveEffect(Player* p){
+}
+
+//================================================================
+// INVISIBLE
+//================================================================
+Invisible::Invisible(float time):Effect(time){
+}
+
+void Invisible::ApplyEffect(Player* p){
+}
+
+void Invisible::RemoveEffect(Player* p){
+}
