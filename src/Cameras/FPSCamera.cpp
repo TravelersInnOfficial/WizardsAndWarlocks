525413f1dd45572551a7c167e15ca7c1a40d832d
angelnm <angel.nm@hotmail.es>
2018-02-27 23:41:52 +0100

    CameraFPS

--- src/Cameras/FPSCamera.cpp
+++ src/Cameras/FPSCamera.cpp
@@ -1,16 +1,73 @@
 #include "FPSCamera.h"
 #include <GraphicEngine/GraphicEngine.h>
 
-FPSCamera::FPSCamera(float rotateSpeed, float moveSpeed){
-    p_Camera = GraphicEngine::getInstance()->addCameraSceneNodeFPS(rotateSpeed, moveSpeed);
+FPSCamera::FPSCamera(vector3df position, vector3df rotation){
+	vector3df target = position;
+	float max = 1;
+	target.X = target.X + sin(rotation.Y)*cos(rotation.X)*max;
+	target.Y = target.Y + sin(rotation.X)*max;
+	target.Z = target.Z + cos(rotation.Y)*cos(rotation.X)*max;
+
+    p_Camera = GraphicEngine::getInstance()->addCameraSceneNode(position, target);
+
+    changeX = 0;
+    changeY = 0;
+    sensibility = 0.005;
 }
 
 FPSCamera::~FPSCamera(){
 }
 
 void FPSCamera::UpdateCamera(vector3df position){
-    vector3df newRot = p_Camera->getRotation();
+    vector3df rotation = p_Camera->getRotation();
 	p_Camera->setPosition(position);
-	p_Camera->updateAbsolutePosition();
-	p_Camera->setRotation(newRot);
+
+
+	vector3df target = position;
+	float max = 1;
+	target.X = target.X + sin(rotation.Y)*cos(rotation.X)*max;
+	target.Y = target.Y + sin(rotation.X)*max;
+	target.Z = target.Z + cos(rotation.Y)*cos(rotation.X)*max;
+
+	p_Camera->setTarget(target);
+
+	CatchMouseInput();
+	vector3df newRotation = GetNewRotation(rotation);
+
+	p_Camera->setRotation(newRotation);
+}
+
+
+vector3df FPSCamera::GetNewRotation(vector3df rotation){
+	rotation.Y += changeX * sensibility;
+	rotation.X += changeY * sensibility;
+
+	float variation = M_PI/16;
+	float max = M_PI/2 - variation;
+	float min = -M_PI/2 + variation;
+	rotation.X = rotation.X>max? max: rotation.X<min? min: rotation.X;
+
+
+	return rotation;
+}
+
+void FPSCamera::CatchMouseInput(){
+	GraphicEngine* engine = GraphicEngine::getInstance();
+	int screenW = engine->GetScreenWidth();
+	int screenH = engine->GetScreenHeight();
+	
+	vector2di mousePos = engine->GetCursorPosition();
+	vector2di dirMouse = lastPos - mousePos;
+
+	lastPos = mousePos;
+	if(lastPos.X<=0 || lastPos.Y<=0 || lastPos.X>=screenW || lastPos.Y>=screenH){
+		lastPos = vector2di(screenW/2, screenH/2);
+		engine->SetCursorPosition(lastPos);
+	}
+
+	// Update the Y angle
+	changeY = dirMouse.Y;
+
+	// Update the X angle
+	changeX = -dirMouse.X;
 }
--- src/Cameras/FPSCamera.h
+++ src/Cameras/FPSCamera.h
@@ -2,14 +2,24 @@
 #define FPSCAMERA_H
 
 #include "Camera.h"
+#include <vector2d.h>
 
 class FPSCamera : public Camera{
 public:
-    FPSCamera(float rotateSpeed, float moveSpeed);
+    FPSCamera(vector3df position, vector3df rotation);
     ~FPSCamera();
 
     void UpdateCamera(vector3df position);
+private:
+	vector3df GetNewRotation(vector3df rotation);
+	void CatchMouseInput();
 
+	vector2di lastPos;      // Ultima posicion en la que se ha encontrado el raton
+
+	int changeX;
+	int changeY;
+
+	float sensibility;
 };
 
 #endif
