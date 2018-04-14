b8f94daf25a33d6d583960a24f25267d5f1673a8
DanielFinestrat <danielfinestratmartinez@hotmail.com>
2018-04-14 14:42:36 +0200

    Better projectile particles

--- src/Engines/GraphicEngine/Particles/ProjectileParticle.cpp
+++ src/Engines/GraphicEngine/Particles/ProjectileParticle.cpp
@@ -8,14 +8,16 @@ ProjectileParticle::~ProjectileParticle(){
 
 void ProjectileParticle::InitParticle(Particle& p){
 
-	float X = (rand() % 10)/10.0f - 0.5f;
-	float Y = (rand() % 10)/10.0f - 0.5f;
-	float Z = (rand() % 10)/10.0f - 0.5f;
-	p.pos 	= toe::core::TOEvector3df(X, Y, Z);
+	int maxPosOffset = 7;
+	float X = (rand() % maxPosOffset)/10.0f - maxPosOffset/20.0f;
+	float Y = (rand() % maxPosOffset)/10.0f - maxPosOffset/20.0f;
+	float Z = (rand() % maxPosOffset)/10.0f - maxPosOffset/20.0f;
+	p.pos = toe::core::TOEvector3df(X, Y, Z);
 
-	Y = (rand() % 2)/10.0f;
-	X = (rand() % 2)/10.0f;
-	Z = (rand() % 2)/10.0f;
+	int maxVel = 5;
+	X = (rand() % maxVel)/10.0f - maxVel/20.0f;
+	Y = (rand() % maxVel)/10.0f - maxVel/20.0f;
+	Z = (rand() % maxVel)/10.0f - maxVel/20.0f;
 	p.speed = toe::core::TOEvector3df(X,Y,Z);
 
 	//int color = (unsigned char)(rand() % 255 + 240);
