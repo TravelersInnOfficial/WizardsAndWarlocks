026aff5ac6ad39cad3321a77cf4d357413246969
angelnm <angel.nm@hotmail.es>
2018-05-02 17:12:54 +0200

    ResourceManager

--- src/ResourceManager.h
+++ src/ResourceManager.h
@@ -1,16 +1,13 @@
 #ifndef RESOURCEMANAGER_H
 #define RESOURCEMANAGER_H
 
-#include <iostream>
-#include <map>
-#include <Assets.h>
-#include <vector3d.h>
-
 class ResourceManager{
 
 public:
     ResourceManager();
     static void LoadResources();
+    static void ReadTextures();
+    static void ReadMeshes();
 };
 
 #endif
