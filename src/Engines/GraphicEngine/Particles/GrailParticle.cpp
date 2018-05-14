687af9bd7e0b80c407657bfb6090f618a3ef8481
Daniel Finestrat <danielfinestratmartinez@hotmail.com>
2018-05-14 12:25:11 +0200

    Grail particles donw

---
+++ src/Engines/GraphicEngine/Particles/GrailParticle.cpp
@@ -0,0 +1,40 @@
+#include "./GrailParticle.h"
+
+GrailParticle::GrailParticle(){
+}
+
+GrailParticle::~GrailParticle(){
+}
+
+void GrailParticle::InitParticle(Particle& p){
+
+	int maxPosOffset = 50;
+	int maxPosOffsetY = 2;
+	float X = (rand() % maxPosOffset)/10.0f - maxPosOffset/20.0f;
+	float Y = (rand() % maxPosOffsetY)/10.0f - maxPosOffsetY/20.0f;
+	float Z = (rand() % maxPosOffset)/10.0f - maxPosOffset/20.0f;
+	p.pos =  TOEvector3df(X, Y, Z);
+
+	Y = (rand() % 2)/10.0f;
+	X = (rand() % 2)/10.0f;
+	Z = (rand() % 2)/10.0f;
+	p.speed =  TOEvector3df(X,Y,Z);
+
+	int color = (unsigned char)(rand() % 255 + 240);
+	p.r = color;
+	p.g = color;
+	p.b = color;
+	
+	// SIZE
+	int maxSize = 5;
+	p.size = (rand() % maxSize)/10.0f;
+
+	p.translation =  TOEvector3df(0,0,0);
+	p.rotation = (rand() % 360);
+	p.life = 9.5f;
+}
+
+void GrailParticle::UpdateParticle(Particle& p, float deltaTime){
+	p.speed.Y += deltaTime * 0.5f * 0.25f;
+	p.pos.Y += p.speed.Y * deltaTime;
+}
